26bf28bc17041d6327d2bf5f0da93dae2df6da00
wzqwangyoucao <1737481406@qq.com>
2018-09-30 00:24:03 +0800

    add git API

--- src/index.js
+++ src/index.js
@@ -5,30 +5,32 @@ const mydata = require('./todos.json')
 // logger
 
 app.use(async (ctx, next) => {
-  console.log('request', ctx.url);
     await next();
-    const rt = ctx.response.get('X-Response-Time');
-      console.log(ctx.request.url==='/');
-      console.log('2');
+    // const rt = ctx.response.get('X-Response-Time');
+    //   console.log(ctx.request.url==='/');
+    //   console.log('2');
+    if(ctx.url=='/getData'){
+        ctx.body = mydata;
+    }
 });
 
 // x-response-time
 
-app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    ctx.set('X-Response-Time', `${ms}ms`);
-    console.log('1');
-});
+// app.use(async (ctx, next) => {
+//     const start = Date.now();
+//     await next();
+//     const ms = Date.now() - start;
+//     ctx.set('X-Response-Time', `${ms}ms`);
+//     console.log('1');
+// });
 
 // response
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-    ctx.body = mydata;
-    console.log('0');
-});
+// app.use(async ctx => {
+//     ctx.body = 'Hello World';
+//     ctx.body = mydata;
+//     console.log('0');
+// });
 
 app.on('error', err => {
     log.error('server error', err)
