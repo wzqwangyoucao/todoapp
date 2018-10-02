c712104118cb839e7b084c3e3451533a76de98f1
wzqwangyoucao <1737481406@qq.com>
2018-10-03 00:48:39 +0800

    action

--- mytodolist/src/index.js
+++ mytodolist/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter,Route,Link,Redirect,Switch} from 'react-router-dom';
+import Mylist from './list.js'
+import './index.css'
 ReactDOM.render(
     <div>
-    123
+        <BrowserRouter>
+                <Switch>
+                    <Route path="/getData" component={Mylist}></Route>
+                </Switch>
+        </BrowserRouter>
     </div>
     , document.getElementById('root')
 );
---
+++ mytodolist/src/list.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { List } from 'antd';
+import { Form, Icon, Input, Button } from 'antd';
+
+    
+
+
+class Mylist extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+             data:[
+            '吃饭饭',
+            '打豆豆',
+            '看书书',
+            '睡觉觉'
+          ]
+         };
+         this.delist=this.delist.bind(this)
+         this.addlist = this.addlist.bind(this)
+    }
+    delist(item) {
+        this.setState(
+            this.state.data.splice(this.state.data.indexOf(item),1)
+        )
+    }
+    addlist(){
+        this.setState(
+            {
+                data:[...this.state.data,this.state.last]
+            }
+        )
+    }
+    render() {
+        return (
+            <div>
+                <Input className='todilist' placeholder="Basic usage" onChange={(v)=>{if(!(this.state.data.indexOf(v.target.value)>-1&&v.target.value =='')){this.setState({...this.state,last:v.target.value})}}}/>
+                <Button type="primary" onClick={()=>this.addlist()}>
+                    增加
+                </Button>
+                <List
+                    className='todilist'
+                    bordered
+                    dataSource={this.state.data}
+                    renderItem={item => (<List.Item actions={[<a onClick={()=>this.delist(item)}>×</a>]}>{item}</List.Item>)}
+                />
+
+            </div>
+        );
+    }
+}
+
+export default Mylist;
